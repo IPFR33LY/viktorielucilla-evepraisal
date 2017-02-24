eeea06252cfbd6561b4ffdeedf88eacc0d92c512
IPFR33LY <mngamingpro@gmail.com>
2017-02-24 15:03:29 -0600

    Testing Loading Scripts

---
+++ evepraisal/static/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nihanke on 2/24/2017.
+ */
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'complete') {
+      setTimeout(function(){
+          document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+      },1000);
+  }
+}
